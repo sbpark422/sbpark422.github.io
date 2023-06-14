44af646d102e5db81c793185c776af4926db4c76
Park, Soo Bin <sbpark422@naver.com>
2023-06-14 13:10:20 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,43 +136,34 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-// 
-// // Set the portfolio page as the default active page on load
-// window.onload = function() {
-//   for (let i = 0; i < pages.length; i++) {
-//     if (pages[i].dataset.page === 'portfolio') {
-//       pages[i].classList.add('active');
-//       navigationLinks[i].classList.add('active');
-//     }
-//   }
-// }
-//
-// // Set the portfolio page as the default active page
-// for (let i = 0; i < pages.length; i++) {
-//   if (pages[i].dataset.page === 'portfolio') {
-//     pages[i].classList.add('active');
-//     navigationLinks[i].classList.add('active');
-//     break;
-//   }
-// }
+document.addEventListener("DOMContentLoaded", function() {
+  // page navigation variables
+  const navigationLinks = document.querySelectorAll("[data-nav-link]");
+  const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
+  // Set the portfolio page as the default active page on load
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].dataset.page === 'portfolio') {
+      pages[i].classList.add('active');
+      navigationLinks[i].classList.add('active');
     }
+  }
 
-  });
-}
+  // add event to all nav link
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+
+      for (let i = 0; i < pages.length; i++) {
+        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+          pages[i].classList.add("active");
+          navigationLinks[i].classList.add("active");
+          window.scrollTo(0, 0);
+        } else {
+          pages[i].classList.remove("active");
+          navigationLinks[i].classList.remove("active");
+        }
+      }
+
+    });
+  }
+});
