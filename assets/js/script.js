bf64ac7136f08e3d9f66fc50192089a4bfe1718d
Park, Soo Bin <sbpark422@naver.com>
2023-06-14 14:24:57 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,42 +16,42 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// // testimonials variables
-// const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-// const modalContainer = document.querySelector("[data-modal-container]");
-// const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-// const overlay = document.querySelector("[data-overlay]");
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
 
-// // modal variable
-// const modalImg = document.querySelector("[data-modal-img]");
-// const modalTitle = document.querySelector("[data-modal-title]");
-// const modalText = document.querySelector("[data-modal-text]");
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
 
-// // modal toggle function
-// const testimonialsModalFunc = function () {
-//   modalContainer.classList.toggle("active");
-//   overlay.classList.toggle("active");
-// }
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
 
-// // add click event to all modal items
-// for (let i = 0; i < testimonialsItem.length; i++) {
-//
-//   testimonialsItem[i].addEventListener("click", function () {
-//
-//     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-//     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-//     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-//     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-//
-//     testimonialsModalFunc();
-//
-//   });
-//
-// }
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
 
-// // add click event to modal close button
-// modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-// overlay.addEventListener("click", testimonialsModalFunc);
+  testimonialsItem[i].addEventListener("click", function () {
+
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+
+    testimonialsModalFunc();
+
+  });
+
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
@@ -115,67 +115,44 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 
-// // contact form variables
-// const form = document.querySelector("[data-form]");
-// const formInputs = document.querySelectorAll("[data-form-input]");
-// const formBtn = document.querySelector("[data-form-btn]");
-//
-// // add event to all form input field
-// for (let i = 0; i < formInputs.length; i++) {
-//   formInputs[i].addEventListener("input", function () {
-//
-//     // check form validation
-//     if (form.checkValidity()) {
-//       formBtn.removeAttribute("disabled");
-//     } else {
-//       formBtn.setAttribute("disabled", "");
-//     }
-//
-//   });
-// }
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
 
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   // page navigation variables
-//   const navigationLinks = document.querySelectorAll("[data-nav-link]");
-//   const pages = document.querySelectorAll("[data-page]");
-//
-//   // Set the portfolio page as the default active page on load
-//   for (let i = 0; i < pages.length; i++) {
-//     if (pages[i].dataset.page === 'portfolio') {
-//       pages[i].classList.add('active');
-//       navigationLinks[i].classList.add('active');
-//     }
-//   }
-//
-//   // add event to all nav link
-//   for (let i = 0; i < navigationLinks.length; i++) {
-//     navigationLinks[i].addEventListener("click", function () {
-//
-//       for (let i = 0; i < pages.length; i++) {
-//         if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-//           pages[i].classList.add("active");
-//           navigationLinks[i].classList.add("active");
-//           window.scrollTo(0, 0);
-//         } else {
-//           pages[i].classList.remove("active");
-//           navigationLinks[i].classList.remove("active");
-//         }
-//       }
-//
-//     });
-//   }
-// });
 
 document.addEventListener("DOMContentLoaded", function() {
   // page navigation variables
   const navigationLinks = document.querySelectorAll("[data-nav-link]");
   const pages = document.querySelectorAll("[data-page]");
 
+  // Set the portfolio page as the default active page on load
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].dataset.page === 'portfolio') {
+      pages[i].classList.add('active');
+      navigationLinks[i].classList.add('active');
+    }
+  }
+
   // add event to all nav link
   for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener("click", function () {
+
       for (let i = 0; i < pages.length; i++) {
         if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
           pages[i].classList.add("active");
@@ -186,13 +163,7 @@ document.addEventListener("DOMContentLoaded", function() {
           navigationLinks[i].classList.remove("active");
         }
       }
+
     });
   }
-
-  // Set the portfolio page as the active page on load
-  const portfolioPage = document.querySelector("[data-page='portfolio']");
-  const portfolioNavLink = [...navigationLinks].find(navLink => navLink.textContent.toLowerCase() === 'portfolio');
-
-  portfolioPage.classList.add('active');
-  portfolioNavLink.classList.add('active');
 });
